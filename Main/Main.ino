ff55d4c649151ac70f9ac1149aea7a72b318f0e7
Spectre <dustin.ngauv@hispeed.ch>
2023-01-05 04:47:54 +0100

    commit

--- Main/Main.ino
+++ Main/Main.ino
@@ -3,14 +3,15 @@
 #include <URTouch.h>
 #include <UTFT_Buttons.h>
 
-UTFT    myGLCD(CTE50,38,39,40,41); //display chip parameters
-URTouch  myTouch( 6, 5, 4, 3, 2); //touch calibration parameters
-UTFT_Buttons  myButtons(&myGLCD, &myTouch);
+UTFT myGLCD(CTE50, 38, 39, 40, 41);  //display chip parameters
+URTouch myTouch(6, 5, 4, 3, 2);      //touch calibration parameters
+UTFT_Buttons myButtons(&myGLCD, &myTouch);
 
 //declares the font used
 extern uint8_t SmallFont[];
-extern uint8_t BigFont[]; 
-extern uint8_t SevenSegNumFont[];
+extern uint8_t BigFont[];
+extern uint8_t SevenSeg_XXXL_Num[];
+extern uint8_t SevenSegment96x144Num[];
 extern uint8_t GroteskBold24x48[];
 
 
@@ -21,11 +22,27 @@ int Level_btn;
 int Rest_btn;
 int Volume_btn;
 int Start_btn;
+int StopSpar_btn;
+
+int rounds = 2;
+int speed = 75;
+long time = 8000;
+int level = 1;
+long rest = 30000;
+int volume = 2;
 
 void drawHomeScreen();
+void drawRoundsScreen();
+void drawSpeedScreen();
+void drawTimeScreen();
+void drawLevelScreen();
+void drawRestScreen();
+void drawVolumeScreen();
 void drawSparringScreen();
 
-Servo servo1; 
+String millisToTimeStr(int milliseconds);
+
+Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
@@ -38,9 +55,10 @@ void setup() {
 
   myTouch.InitTouch();
   myTouch.setPrecision(PREC_MEDIUM);
-  
 }
 
-void loop() {  
-  drawSparringScreen(); 
+void loop() {
+  drawHomeScreen();
+  //drawRoundsScreen();
+  //drawSparringScreen();
 }
--- Main/a_Screens.ino
+++ Main/a_Screens.ino
@@ -1,41 +1,14 @@
 void drawHomeScreen() {
-  myGLCD.setBackColor(0, 0, 0);
-  myButtons.setTextFont(BigFont);
-  myButtons.setButtonColors(VGA_WHITE, VGA_GRAY, 0xE800, VGA_WHITE, 0xE800);
-
-  int Sparring_btn;
-  int Practice_btn;
-
-  Sparring_btn = myButtons.addButton(250, 150, 300, 50, "Sparring Mode");
-  Practice_btn = myButtons.addButton(250, 250, 300, 50, "Practice Mode");
-
-  myButtons.drawButtons();
-
-  while (true) {
-    if (myTouch.dataAvailable()) {
-      int pressed = myButtons.checkButtons();
-
-      if (pressed == Sparring_btn) {        
-
-        myGLCD.clrScr();
-        myButtons.deleteAllButtons();  
-        drawSparringScreen();           
-      }
-    }
-  }
-}
-
-
-void drawSparringScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
   myGLCD.setBackColor(0, 0, 0);
   myGLCD.setColor(255, 255, 255);
   myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Sparring Mode", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+
   myButtons.setTextFont(GroteskBold24x48);
   myButtons.setButtonColors(VGA_WHITE, VGA_GRAY, VGA_BLACK, VGA_WHITE, VGA_RED);
-    
-  myGLCD.print("Sparring Mode", CENTER, 15); //draws title
-  myGLCD.drawLine(0, 80, 800, 80);
-  
 
   Rounds_btn = myButtons.addButton(200, 100, 150, 60, "");
   Speed_btn = myButtons.addButton(450, 100, 150, 60, "");
@@ -43,17 +16,15 @@ void drawSparringScreen() {
   Level_btn = myButtons.addButton(450, 200, 150, 60, "");
   Rest_btn = myButtons.addButton(200, 300, 150, 60, "");
   Volume_btn = myButtons.addButton(450, 300, 150, 60, "");
-   
-
   myButtons.drawButtons();
 
-  myButtons.setButtonColors(VGA_WHITE, VGA_GRAY, VGA_BLACK, VGA_WHITE, 0x04A7);  
+  myButtons.setButtonColors(VGA_WHITE, VGA_GRAY, VGA_BLACK, VGA_WHITE, 0x04A7);
   Start_btn = myButtons.addButton(300, 380, 200, 80, "START");
   myButtons.drawButton(Start_btn);
 
-  
+
   myGLCD.setFont(SmallFont);
-  myGLCD.setBackColor(235, 0, 0);
+  myGLCD.setBackColor(255, 0, 0);
   myGLCD.print("Rounds", 253, 105);
   myGLCD.print("Speed", 506, 105);
   myGLCD.print("Time", 260, 205);
@@ -62,18 +33,154 @@ void drawSparringScreen() {
   myGLCD.print("Strike Volume", 475, 305);
 
   myGLCD.setFont(BigFont);
-  myGLCD.print("", 270, 125);
+  myGLCD.printNumI(rounds, 270, 125);
+  myGLCD.print(String(speed) + "%", 508, 125);
+  myGLCD.print(millisToTimeStr(time), 245, 225);
+  myGLCD.printNumI(level, 521, 225);
+  myGLCD.print(millisToTimeStr(rest), 245, 325);
+  myGLCD.printNumI(volume, 520, 325);
 
   while (true) {
     if (myTouch.dataAvailable()) {
       int pressed = myButtons.checkButtons();
 
-      if (pressed == Rounds_btn) {                    
-        myGLCD.clrScr();
-        myButtons.deleteAllButtons();
-        drawHomeScreen();
-        
+      if (pressed == Rounds_btn) {
+        drawRoundsScreen();
+      } else if (pressed == Speed_btn) {
+        drawSpeedScreen();
+      } else if (pressed == Time_btn) {
+        drawTimeScreen();
+      } else if (pressed == Level_btn) {
+        drawLevelScreen();
+      } else if (pressed == Rest_btn) {
+        drawRestScreen();
+      } else if (pressed == Volume_btn) {
+        drawVolumeScreen();
+      } else if (pressed == Start_btn) {
+        drawSparringScreen();
       }
     }
   }
+}
+
+void drawRoundsScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Choose Rounds", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+
+  myGLCD.setFont(SevenSeg_XXXL_Num);
+  myGLCD.printNumI(rounds, CENTER, 200);
+}
+
+void drawSpeedScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Adjust Speed", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+}
+
+void drawTimeScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Choose Time", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+}
+
+void drawLevelScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Choose Level", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+}
+
+void drawRestScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Choose Rest", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+}
+
+void drawVolumeScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+  myGLCD.print("Choose Strike Volume", CENTER, 15);  //draws title
+  myGLCD.drawLine(0, 80, 800, 80);
+}
+
+void drawSparringScreen() {
+  myGLCD.clrScr();
+  myButtons.deleteAllButtons();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.setFont(GroteskBold24x48);
+
+  long initialseconds = millis() / 1000;
+  long countdowntime; 
+  long start_time = 0;
+
+  myButtons.setTextFont(GroteskBold24x48);
+  myButtons.setButtonColors(VGA_WHITE, VGA_GRAY, VGA_BLACK, VGA_WHITE, VGA_RED);
+  StopSpar_btn = myButtons.addButton(300, 380, 200, 80, "STOP");
+  myButtons.drawButtons();
+  
+
+  for (int i = 0; i < rounds; i++) {
+    myGLCD.print("Round " + String(i + 1) + "/" + String(rounds), CENTER, 50);
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.fillRoundRect(100, 315, 700, 300);  
+    
+
+    while (true) {      
+      countdowntime = (time / 1000) + initialseconds + start_time - (millis() / 1000);
+      long countdown_minute = ((countdowntime / 60) % 60);
+      long countdown_sec = countdowntime % 60;
+      float progress = 1 - (countdowntime / float(time / 1000));
+      myGLCD.print(String(countdown_minute) + ":" + String(countdown_sec), CENTER, 200);      
+      myGLCD.setColor(0, 26, 255);
+      myGLCD.fillRoundRect(100, 315, 100 + progress*600, 300);
+      myGLCD.setColor(255, 255, 255);
+
+      if (countdowntime == 0) {        
+        break;
+      }        
+      delay(100);
+    }  
+      
+    if (countdowntime == 0 && (i + 1) == rounds) {
+      delay(1000);
+      drawHomeScreen();
+      break;
+    }
+
+
+    start_time = (time / 1000) * (i+1);      
+  }    
+}
+
+String millisToTimeStr(long milliseconds) {
+  String minutes = String(milliseconds / 60000);
+  String seconds = String((milliseconds / 1000) % 60);
+  if (seconds.length() < 2) {
+    seconds = "0" + seconds;
+  }
+  return minutes + ":" + seconds;
 }
